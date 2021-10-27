a4be8d27281e1524316c2798053ea13f676975ad
Relicjamin-jv <campbellcl19@gcc.edu>
2021-10-26 22:07:58 -0400

    Added simple Animation
    all animations in place

--- Assets/Scripts/Player/playerScript.cs
+++ Assets/Scripts/Player/playerScript.cs
@@ -9,19 +9,43 @@ public class playerScript : MonoBehaviour
     private Rigidbody2D _ridgidBody;
     private Transform _transform;
     public LayerMask _groundLayer;
+    public Animator _animator;
+    public bool isLookingRight = false;
+    private SpriteRenderer _spriteRenderer;
 
     private void Start() {
         _ridgidBody = GetComponent<Rigidbody2D>();
         _transform = this.transform;
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update() {
         float movement = Input.GetAxis("Horizontal");
+
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            isLookingRight = true;
+        }
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            isLookingRight = false;
+        }
+
+        _spriteRenderer.flipX = isLookingRight;
+
         transform.position += new Vector3(movement, 0 ,0) * Time.deltaTime * movementSpeed;
 
-        if(Input.GetKeyDown(KeyCode.W) && isGrounded()){
+        if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded()){
             _ridgidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
+        
+
+        Debug.Log(isGrounded());
+
+        _animator.SetBool("isJumping", !isGrounded());
+        _animator.SetFloat("isMoving", Mathf.Abs(movement));
+       
     }
 
     private bool isGrounded(){
